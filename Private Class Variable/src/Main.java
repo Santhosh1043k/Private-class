94af126a51b4029b35aef1f1810d064df03ddc5f
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-13 18:14:51 +0530

    Add files via upload

---
+++ Private Class Variable/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Shop shop =new Shop();
+
+        //Shop is class and shop is object name
+        shop.ithaVachuko(10);
+        shop.ithaVachuko(100);
+        shop.ithaVachuko(100);
+
+        System.out.println(shop.evaloKoduthe());
+
+    }
+}
---
+++ Private Class Variable/src/Shop.java
@@ -0,0 +1,11 @@
+public class Shop {
+    private int money;
+    //in this line we use the private class and declare that pri class as integer
+    public void ithaVachuko(int money){
+        this.money += money;
+
+    }
+    public int evaloKoduthe(){
+        return money;
+    }
+}
